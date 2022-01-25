0e98bdeba1c8a6aff3c3a922f59f3a20dfdf84fe
seongjin_song <seongjin_song@tmax.co.kr>
2022-01-26 00:50:48 +0900

    공통 인터페이스 설정

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,8 +2,10 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@EnableJpaRepositories(basePackages = "study.datajpa.repository") // 부트에 포함되어있어서 명시안해도 된다.
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/study/datajpa/repository/TeamRepository.java
+++ src/main/java/study/datajpa/repository/TeamRepository.java
@@ -1,34 +1,9 @@
 package study.datajpa.repository;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Team;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public class TeamRepository {
-    @PersistenceContext
-    private EntityManager em;
-    public Team save(Team team){
-        em.persist(team);
-        return team;
-    }
-    public void delete(Team team){
-        em.remove(team);
-    }
-    public List<Team> findAll(){
-        return em.createQuery("select t from Team t", Team.class)
-                .getResultList();
-    }
-    public Optional<Team> findById(Long id){
-        Team team = em.find(Team.class, id);
-        return Optional.ofNullable(team);
-    }
-    public long count(){
-        return em.createQuery("select count(t) from Team t", Long.class)
-                .getSingleResult();
-    }
+//@Repository 애노테이션을 생략해도 된다.
+// Component 스캔 및 JPA 예외를 스프링 공통적으로 처리할수 있게하는 애노테이션
+public interface TeamRepository extends JpaRepository<Team, Long> {
 }

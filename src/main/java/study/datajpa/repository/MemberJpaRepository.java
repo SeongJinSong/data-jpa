6a2badb88de83f5fce4ebba0afb2a370d3ba3929
seongjin_song <seongjin_song@tmax.co.kr>
2022-01-25 22:55:10 +0900

    순수 JPA 기반 리포지토리 만들기

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -6,6 +6,8 @@ import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -15,6 +17,26 @@ public class MemberJpaRepository {
     public Member save(Member member){
         em.persist(member);
         return member;
+    }
+
+    public void delete(Member member){
+        em.remove(member);
+    }
+
+    /**
+     * 전체조회 혹은 특정 조건 필터링하는 경우는 JPQL을 사용해야한다.
+     */
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+    public Optional<Member> findById(Long id){
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+    public long count(){
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
     }
     public Member find(Long id){
         return em.find(Member.class, id);

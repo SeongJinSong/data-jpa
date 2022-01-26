155a0395d4fbdd54fa4e1b313a3ea11bf2cc210b
seongjin_song <seongjin_song@tmax.co.kr>
2022-01-27 01:07:12 +0900

    Web 확장 - 페이징과 정렬

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 import study.datajpa.repository.MemberRepositoryCustom;
@@ -32,6 +36,28 @@ public class MemberController {
 
     @PostConstruct
     public void init(){
-        memberRepository.save(new Member("userA"));
+        //memberRepository.save(new Member("userA"));
+        for(int i=0;i<100;i++){
+            memberRepository.save(new Member("user"+i, i));
+        }
+    }
+
+    /**
+     * 페이징과 정렬
+     */
+    @GetMapping("/v1/members")
+    public Page<Member> list(@PageableDefault(size=5)  Pageable pageable){
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page;
+    }
+
+    /**
+     * 꼭 Dto로 반환하자
+     * Entity로 반환하면 가까운 미레에 후회를 하게 될 것이다!
+     */
+    @GetMapping("/v2/members")
+    public Page<MemberDto> list2(@PageableDefault(size=5)  Pageable pageable){
+        return memberRepository.findAll(pageable)
+                .map(MemberDto::new);
     }
 }

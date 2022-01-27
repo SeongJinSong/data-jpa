7d6dc4843cd2761d79d94425d3c7f06b9e79c828
seongjin_song <seongjin_song@tmax.co.kr>
2022-01-27 13:11:32 +0900

    네이티브 쿼리

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -503,8 +503,51 @@ class MemberRepositoryTest {
         assertThat(result.get(0).getUsername()).isEqualTo("m1");
     }
 
+//    @Test
+//    public void CloseIfProjections(){
+//        //given
+//        Team teamA = new Team("teamA");
+//        em.persist(teamA);
+//
+//        Member m1 = new Member("m1", 0, teamA);
+//        Member m2 = new Member("m2", 0, teamA);
+//        em.persist(m1);
+//        em.persist(m2);
+//
+//        em.flush();
+//        em.clear();
+//
+//        //when
+//        List<UsernameOnlyDto> result = memberRepository.findProjectionsByUsername("m1", UsernameOnlyDto.class);
+//        for (UsernameOnlyDto usernameOnly : result) {
+//            System.out.println("usernameOnly = " + usernameOnly);
+//        }
+//    }
+
+//    @Test
+//    public void NestedProjections(){
+//        //given
+//        Team teamA = new Team("teamA");
+//        em.persist(teamA);
+//
+//        Member m1 = new Member("m1", 0, teamA);
+//        Member m2 = new Member("m2", 0, teamA);
+//        em.persist(m1);
+//        em.persist(m2);
+//
+//        em.flush();
+//        em.clear();
+//
+//        //when
+////        List<UsernameOnlyDto> result = memberRepository.findProjectionsByUsername("m1", UsernameOnlyDto.class);
+//        List<NestedClosedProjections> result = memberRepository.findProjectionsByUsername("m1", NestedClosedProjections.class);
+//        for (NestedClosedProjections usernameOnly : result) {
+//            System.out.println("usernameOnly = " + usernameOnly);
+//        }
+//    }
+
     @Test
-    public void CloseIfProjections(){
+    void findByNativeQuery() {
         //given
         Team teamA = new Team("teamA");
         em.persist(teamA);
@@ -518,14 +561,12 @@ class MemberRepositoryTest {
         em.clear();
 
         //when
-        List<UsernameOnlyDto> result = memberRepository.findProjectionsByUsername("m1", UsernameOnlyDto.class);
-        for (UsernameOnlyDto usernameOnly : result) {
-            System.out.println("usernameOnly = " + usernameOnly);
-        }
+        Member result = memberRepository.findByNativeQuery("m1");
+        System.out.println("result = " + result);
     }
 
     @Test
-    public void NestedProjections(){
+    void findByNativeProjection() {
         //given
         Team teamA = new Team("teamA");
         em.persist(teamA);
@@ -539,10 +580,11 @@ class MemberRepositoryTest {
         em.clear();
 
         //when
-//        List<UsernameOnlyDto> result = memberRepository.findProjectionsByUsername("m1", UsernameOnlyDto.class);
-        List<NestedClosedProjections> result = memberRepository.findProjectionsByUsername("m1", NestedClosedProjections.class);
-        for (NestedClosedProjections usernameOnly : result) {
-            System.out.println("usernameOnly = " + usernameOnly);
+        Page<MemberProjection> result = memberRepository.findByNativeProjection(PageRequest.of(0, 10));
+        List<MemberProjection> content = result.getContent();
+        for (MemberProjection memberProjection : content) {
+            System.out.println("memberProjection.getUsername() = " + memberProjection.getUsername());
+            System.out.println("memberProjection.getTeamName() = " + memberProjection.getTeamName());
         }
     }
 }
